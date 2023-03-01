43d1fd23812cad107a581b2ad1459d686ef38ff0
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-03-01 16:51:21 -0700

    Revert "remov .env from gitignore"
    This reverts commit 8908b36a7b4359a980dbca95aa4be8b02cbc6c15.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :users, :repo_name, :pull_requests
+
+  def pull_requests
+    @pull_requests = PullRequestSearch.new.merged_pr_count
+  end
+
+	def users
+		@users = UsersSearch.new.usernames_and_commits
+	end
+
+  def repo_name
+    @repo_name = RepoNameSearch.new.repo_name_information
+  end
 
 	private
-
 	def error_message(errors)
 		errors.full_messages.join(', ')
 	end
