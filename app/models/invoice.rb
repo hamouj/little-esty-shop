39005fb38772f8cb0eb481b99f98ddf47f3cef2e
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-03-07 09:57:39 -0800

    Fix: add bulk_discount name for tests and fix formatting

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -9,7 +9,11 @@ class Invoice < ApplicationRecord
   enum status: ["cancelled", "in progress", "completed"]
 
   def self.invoice_items_not_shipped
-    joins(:invoice_items).where.not(invoice_items: {status: 2}).distinct.order(:created_at)
+    joins(:invoice_items)
+    .where
+    .not(invoice_items: {status: 2})
+    .distinct
+    .order(:created_at)
   end
 
   def items_for_merchant(merchant)
